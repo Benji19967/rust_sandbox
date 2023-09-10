3fab810c2e0c6a3f4595775b60cd0391d9d799cb
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-09-10 12:41:45 -0300

    Add more functions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    // Hello World!
-    // println!("Hello World!");
-
-
+    println!("Hello World!");
     print_hardcoded_string();
     print_an_integer(7);
     print_height(188, "cm");
+    println!("{}", five());
+    println!("5 + 1 is: {}", plus_one(5));
 }
 
 fn print_hardcoded_string() {
@@ -18,4 +17,12 @@ fn print_an_integer(x: i32) {
 
 fn print_height(height: i32, unit: &str) {
     println!("The height is: {height}{unit}")
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
