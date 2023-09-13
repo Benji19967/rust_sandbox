fc9439b53963d985ab82cde9c6cdc1aa4cc1648c
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-09-13 17:25:26 -0300

    Get word counts and print frequency of each word

--- src/bin/count_words.rs
+++ src/bin/count_words.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashMap;
 
 use clap::Parser;
 
@@ -10,6 +11,10 @@ struct Args {
     path: String,
 }
 
+fn remove_punctuation(s: &str) -> String {
+    s.replace(&['(', ')', ',', '\"', '.', ';', ':', '\'', '*'][..], "")
+}
+
 fn main() {
     // Read CLI input: which file are we counting words in?
     let args = Args::parse();
@@ -17,20 +22,29 @@ fn main() {
     // Read file content into a string
     let data = fs::read_to_string(&args.path).expect("Unable to read file");
 
-    let mut counter: u32 = 0;
-
     // Create a Hashmap for words + counts 
+    // TODO: How can I make the keys `&str`?
+    let mut word_counts: HashMap<String, u32> = HashMap::new();
+
+    // Iterate string and populate hashmap
     for line in data.trim().lines() {
         for word in line.split_whitespace() {
-            println!("{}", word);
-            counter += 1;
-            if counter == 100 {
-                return;
+            let word_str = remove_punctuation(&word);
+            if !word_str.is_empty() {
+                println!("{}", word_str);
             }
+            let entry = word_counts.entry(word_str).or_insert(0);
+            *entry += 1;
+
         }
     }
 
-    // Iterate string and populate hashmap
-
     // Print out results
+    // TODO: Print in order. Could use a Tree Map to keep words sorted.
+    // TODO: Or if we only print the k most common ones we don't need to sort but can
+    // just iterate the values and keep the most common ones.
+    for (k, v) in word_counts.iter() {
+        println!("{k}: {v}");
+    }
+
 }
