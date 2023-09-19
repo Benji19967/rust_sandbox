b23173ca70a6919aab9b77c3e56042018d4f2be0
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-09-19 17:01:38 -0300

    Add option to only print most frequent k words

--- src/bin/count_words.rs
+++ src/bin/count_words.rs
@@ -5,6 +5,7 @@ use clap::Parser;
 use rust_sandbox::my_io::read_lines;
 
 // See questions unders `/docs/COUNT_WORDS.md`
+
 // TODO: Make the program multi-threaded and count the word frequency for many
 // files. Aggregate the frequency of each word into one count at the end.
 // Compare performance between the single-threaded and multi-threaded versions.
@@ -14,6 +15,9 @@ struct Args {
     /// The path to the file to read
     #[arg(short, long)]
     path: String,
+    /// Find the most common k words
+    #[arg(short = 'k', long = "num-most-frequent")]
+    k: Option<usize>,
 }
 
 fn remove_punctuation(s: &str) -> String {
@@ -21,10 +25,8 @@ fn remove_punctuation(s: &str) -> String {
 }
 
 fn main() {
-    // Read CLI input: which file are we counting words in?
     let args = Args::parse();
 
-    // Create a Hashmap for words + counts
     let mut word_counts: HashMap<String, u32> = HashMap::new();
 
     // Read file content into a string
@@ -48,10 +50,14 @@ fn main() {
     // can just iterate the values and keep the most common ones.
     let mut word_counts_vec: Vec<(String, u32)> = word_counts.into_iter().collect();
     word_counts_vec.sort_by(|(_, cnt1), (_, cnt2)| cnt1.cmp(&cnt2).reverse());
+    let word_counts_vec_most_frequent: Vec<(String, u32)> = match args.k {
+        Some(k) => word_counts_vec[..k].to_vec(),
+        None => word_counts_vec
+    };
 
     // Print in reverse order so we see the most common words at the bottom
     // of the screen.
-    for (word, count) in word_counts_vec.iter().rev() {
+    for (word, count) in word_counts_vec_most_frequent.iter().rev() {
         println!("{word}: {count}");
     }
 
