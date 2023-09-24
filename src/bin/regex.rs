ec2db0dbfcc5bb4259a73893c304de3e0eacff5c
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-09-24 12:41:34 -0300

    Add find_iter examples

--- src/bin/regex.rs
+++ src/bin/regex.rs
@@ -17,7 +17,6 @@ mod tests {
     //
     // is_match: does a pattern match?
     //
-
     #[test]
     fn is_match1() {
         let re = Regex::new(r"Ben").unwrap();
@@ -30,20 +29,37 @@ mod tests {
     }
 
     //
-    // find: get the byte offsets of the match and the actual substring that matched
+    // find: get the byte offsets of the first match and the actual substring that matched
     //
-    
     #[test]
     fn find1() {
         let re = Regex::new(r"Ben").unwrap();
-        let mat = re.find("Ben").unwrap();
+        let mat = re.find("Ben Ben").unwrap();
         assert_eq!(mat.range(), 0..3);
         assert_eq!(mat.as_str(), "Ben");
     }
     #[test]
     fn find2() {
         let re = Regex::new(r"Beno").unwrap();
-        let mat = re.find("Ben");
+        let mat = re.find("Ben Ben");
         assert_eq!(mat, None);
+    }
+    
+    //
+    // find_iter: find all successive non-overlapping matches
+    //
+    #[test]
+    fn find_iter1() {
+        let re = Regex::new(r"Ben").unwrap();
+        let matches_range: Vec<_> = re.find_iter("Ben Ben").map(|x| x.range()).collect();
+        let matches_str: Vec<_> = re.find_iter("Ben Ben").map(|x| x.as_str()).collect();
+        assert_eq!(matches_range, vec![0..3, 4..7]);
+        assert_eq!(matches_str, vec!["Ben", "Ben"]);
+    }
+    #[test]
+    fn find_iter2() {
+        let re = Regex::new(r"Beno").unwrap();
+        let matches: Vec<_> = re.find_iter("Ben Ben").collect();
+        assert_eq!(matches, vec![]);
     }
 }
