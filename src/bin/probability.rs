9f81101df1fabeaef1c8e6b2eefa68c5113276c2
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-09-26 19:37:30 -0300

    Turn probability into a proper CLI

--- src/bin/probability.rs
+++ src/bin/probability.rs
@@ -1,27 +1,30 @@
-use std::io::{self, Write, Read};
+use std::io::{self, Write};
 
+use clap::Parser;
 use rand::Rng;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn main() -> Result<()>{
-    // Game 1: 
-    // There is a 20-sided die on the table with a 1 facing up. 
+#[derive(Parser)]
+struct Args {
+    /// Find the most common k words
+    #[arg(short = 'n', long = "num-simulations", default_value = "1000000")]
+    num_simulations: u32,
+}
+
+fn main() -> Result<()> {
+    // Game 1:
+    // There is a 20-sided die on the table with a 1 facing up.
     // You will be able to play for 100 rounds.
     // Your options for each round are:
     // - get the amount of money the die is showing
     // - reroll
 
-    let cmd = std::env::args();
-    let args = cmd.into_iter().collect::<Vec<_>>();
-    let num_simulations: u32 = args[1].parse()?;
-    // let mut input = String::new();
-    // io::stdin().read_line(&mut input)?;
-    // let num_simulations: u32 = input.trim().parse()?;
+    let args = Args::parse();
 
     for take_if_equal_or_greater_than in 1..21 {
         let mut total_winnings = 0;
-        for _ in 0..num_simulations {
+        for _ in 0..args.num_simulations {
             let mut winnings = 0;
             let mut dice = 1;
 
@@ -34,8 +37,13 @@ fn main() -> Result<()>{
             }
             total_winnings += winnings;
         }
-        let avg_winnings = total_winnings / num_simulations;
-        writeln!(io::stdout(), "Take >={}, Winnings: {}", take_if_equal_or_greater_than, avg_winnings)?;
+        let avg_winnings = total_winnings / args.num_simulations;
+        writeln!(
+            io::stdout(),
+            "Take >={}, Winnings: {}",
+            take_if_equal_or_greater_than,
+            avg_winnings
+        )?;
     }
     Ok(())
 }
